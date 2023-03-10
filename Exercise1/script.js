1a90b6325a0ee0e1f76d476dd9bafcdeab5b667d
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2023-03-10 05:52:46 +0000

    add anthor fuction

--- Exercise1/script.js
+++ Exercise1/script.js
@@ -1,7 +1,19 @@
+// function myFfuction() {
+//     let person = prompt("What's your name", "F");
+//     if (person != null) {
+//       document.getElementById("demo").innerHTML =
+//       `Hello ${person}`;
+//     }
+//   }
+
+// add more JS
 function myFfuction() {
-    let person = prompt("What your name", "F");
-    if (person != null) {
-      document.getElementById("demo").innerHTML =
-      `Hello ${person}`;
-    }
+  let text;
+  let person = prompt("What's your name:", "F");
+  if (person == null || person == "") {
+    text = "User cancelled the prompt.";
+  } else {
+    text = "Hello " + person;
   }
+  document.getElementById("demo").innerHTML = text;
+}
